67fd80ca065d7c3bf9384e365a8ef357117b647e
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-10-12 14:12:49 -0300

    feat: add initial structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css';
+
+export const App = () => {
+
+  return (
+    <div className="App">
+      <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+    </div>
+  )
+}
