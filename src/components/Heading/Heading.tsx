a70c1c15fb05d8bce220581096c7283eed0bf778
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-10-14 16:59:13 -0300

    feat: add prop to optionally add classname to component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,34 @@
+
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+export interface HeadingProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
+}
+
+export const Heading = ({ size = 'md', children, asChild, className }: HeadingProps) => {
+
+ const CustomComponent = asChild ? Slot : 'h2';
+
+  return (
+    <div>
+      <CustomComponent
+        className={clsx(
+          'text-gray-100 font-bold font-sans',
+          {
+            'text-lg': size === 'sm',
+            'text-xl': size === 'md',
+            'text-2xl': size === 'lg',
+          },
+          className
+        )}
+      >
+        {children}
+      </CustomComponent>
+    </div>
+  );
+}
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,32 @@
+
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
+}
+
+export const Text = ({ size = 'md', children, asChild, className }: TextProps) => {
+
+ const CustomComponent = asChild ? Slot : 'span';
+
+  return (
+    <div>
+      <CustomComponent
+        className={clsx('text-gray-100 font-sans', {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        },
+        className,
+        )}
+      >
+        {children}
+      </CustomComponent>
+    </div>
+  );
+}
