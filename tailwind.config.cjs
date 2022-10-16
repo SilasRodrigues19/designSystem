309b5fe7b5175620c7989e88bbb97c488a9bf03c
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-10-15 22:27:31 -0300

    feat: add new tokens for cyan color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
 
-      black: '#000',
+      black: '#1c1c1c',
       white: '#fff',
 
       gray: {
@@ -21,10 +21,12 @@ module.exports = {
         800: '#202024',
         400: '#7c7c8a',
         200: '#c4c4cc',
-        100: '#e1e1e6',
+        100: '#d5d5db',
       },
 
       cyan: {
+        800: '#00A8C9',
+        700: '#00BCE3',
         500: '#81d8f7',
         300: '#9be1fb',
       },
